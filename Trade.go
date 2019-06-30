f0b175cfb4a78a9d838e0ed7db8b375d3c4318bf
Anders Brander <abk@cego.dk>
2019-06-30 09:08:17 +0100

    Some documentation.

--- Trade.go
+++ Trade.go
@@ -12,13 +12,16 @@ type HistoricalTrade struct {
 
 // Trade is a trade matched by the engine.
 type Trade struct {
-	Symbol        Symbol `json:"s"`
-	TradeID       int64  `json:"t"`
-	Price         Value  `json:"p"`
-	Quantity      Value  `json:"q"`
-	BuyerOrderID  int64  `json:"b"`
-	SellerOrderID int64  `json:"a"`
-	Timestamp     Time   `json:"T"`
-	BuyerMaker    bool   `json:"m"`
-	BestMatch     bool   `json:"M"`
+	Symbol Symbol `json:"s"`
+
+	// These match what's seen on a HistoricalTrade.
+	TradeID    int64 `json:"t"`
+	Price      Value `json:"p"`
+	Quantity   Value `json:"q"`
+	Timestamp  Time  `json:"T"`
+	BuyerMaker bool  `json:"m"`
+	BestMatch  bool  `json:"M"`
+
+	BuyerOrderID  int64 `json:"b"`
+	SellerOrderID int64 `json:"a"`
 }
